8c7991949bef80e25d9ad0a1f77817197f033d65
Junx <dungphamdeveloper24@gmail.com>
2026-08-14 15:06:44 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   // Bỏ output: 'export' vì Hostinger đang chạy server Next.js chứ không phải static web
   output: 'standalone', 
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
